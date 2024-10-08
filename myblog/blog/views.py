44ec74a8ccbe39e58b656a44278f89f10e9c030a
SashaNenakhova <86903922+SashaNenakhova@users.noreply.github.com>
2024-10-08 13:46:32 +0500

    blog finished
    added style and comments

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-
 from blog.models import Post, Comment
-
+from django.http import HttpResponseRedirect
+from blog.forms import CommentForm
 
 def blog_index(request):
 
@@ -46,6 +43,28 @@ def blog_category(request, category):
 def blog_detail(request, pk):
 
     post = Post.objects.get(pk=pk)
+    
+    form = CommentForm()
+
+    if request.method == "POST":
+
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+
+            comment = Comment(
+
+                author=form.cleaned_data["author"],
+
+                body=form.cleaned_data["body"],
+
+                post=post,
+
+            )
+
+            comment.save()
+
+            return HttpResponseRedirect(request.path_info)
 
     comments = Comment.objects.filter(post=post)
 
@@ -54,6 +73,8 @@ def blog_detail(request, pk):
         "post": post,
 
         "comments": comments,
+        
+        "form": CommentForm(),
 
     }
 
