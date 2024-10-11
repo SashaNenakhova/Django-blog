1c8f3011b097284cb2ac716ed0da7d12b1ba8eeb
SashaNenakhova <86903922+SashaNenakhova@users.noreply.github.com>
2024-10-11 11:32:42 +0500

    authentification workss
    base.html block page_content
    in login.html block content

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,12 +1,12 @@
 # blog/urls.py
 
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.blog_index, name="blog_index"),
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
     path("category/<category>/", views.blog_category, name="blog_category"),
+    path("", views.blog_index, name="blog_index"),
 ]
 
 
